66cd294b53328708b9707c4a436c8cc02d076910
Sweet-Blood <59991590+Sweet-Blood@users.noreply.github.com>
2021-01-30 23:44:32 +0530

    Add files via upload

---
+++ FirstFile.py
@@ -0,0 +1,5 @@
+print("Hello, Friends")
+print("\nThis is the first python file i am uploading.")
+a=int(input("Enter a Number : "))
+print("Why do you like this number \nBy the way the square of the number is ",(a*a))
+# This is the github test for me. Don't have to see or comment on this code bro....
